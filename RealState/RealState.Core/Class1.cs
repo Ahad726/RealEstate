2c003255d21fcc77aa1d436d742e7d709ab1d283
Ahad <atmahad726@gmail.com>
2023-03-12 15:46:08 +0600

    generic repository added

---
+++ RealState/RealState.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RealState.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ RealState/RealState.Data/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealState.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DbContext _dbContext;
+        private DbSet<T> _dbSet;
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
