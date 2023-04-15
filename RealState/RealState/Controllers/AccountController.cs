b2be8aaf5f55759e4b0c3721a63b97b70e7f3f9d
Ahad <atmahad726@gmail.com>
2023-04-15 16:02:29 +0600

    Role based authorization

--- RealState/RealState/Controllers/AccountController.cs
+++ RealState/RealState/Controllers/AccountController.cs
@@ -2,8 +2,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using RealState.Core.UnitOfWorks;
 using RealState.Models;
+using RealState.SD;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RealState.Controllers
@@ -13,13 +19,123 @@ namespace RealState.Controllers
         private readonly ILogger<AccountController> _logger;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ILogger<AccountController> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AccountController(ILogger<AccountController> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
             _signInManager = signInManager;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
+
+        [HttpGet]
+        [Authorize(Roles =RoleType.Role_Admin)]
+        public IActionResult Register(string returnUrl = null)
+        {
+            var registerModel = new RegisterModel
+            {
+                ReturnUrl = returnUrl,
+                RoleList = _roleManager.Roles.Where(x => x.Name != RoleType.Role_Admin).Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            return View(registerModel);
+
+        }
+
+        [HttpPost]
+        [Authorize(Roles = RoleType.Role_Admin)]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            model.ReturnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+
+                var userExsist = await _userManager.FindByEmailAsync(model.Email);
+
+                if (userExsist == null)
+                {
+                    var newUser = await _userManager.CreateAsync(user, model.Password);
+                    if (newUser.Succeeded)
+                    {
+                        _logger.LogInformation("User created a new account with password.");
+
+                        if (!await _roleManager.RoleExistsAsync(RoleType.Role_Admin))
+                        {
+                            await _roleManager.CreateAsync(new IdentityRole(RoleType.Role_Admin));
+                        }
+                        if (!await _roleManager.RoleExistsAsync(RoleType.Role_Employee))
+                        {
+                            await _roleManager.CreateAsync(new IdentityRole(RoleType.Role_Employee));
+                        }
+
+                        var role = await _roleManager.FindByIdAsync(model.Role);
+
+                        if (role != null)
+                        {
+                            await _userManager.AddToRoleAsync(user, role.Name);
+                        }
+
+
+                        //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                        //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                        //var callbackUrl = Url.Action("Index",
+                        //    "Home",
+                        //    values: new {userId = user.Id, code = code },
+                        //    protocol: Request.Scheme);
+
+
+
+                        //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                        //{
+                        //    return RedirectToAction("RegisterConfirmation", new { email = model.Email });
+                        //}
+                        //else
+                        //{
+                        //    if (user.Role == null)
+                        //    {
+                        //        await _signInManager.SignInAsync(user, isPersistent: false);
+                        //        return LocalRedirect(model.ReturnUrl);
+                        //    }
+                        //    else
+                        //    {
+                        //        //admin is registering new user
+                        //        return RedirectToAction(nameof(Index), "User", new { Area = "Admin" });
+                        //    }
+                        //}
+                    }
+                    foreach (var error in newUser.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+
+                return View();
+
+            }
+
+            // If we got this far, something failed, redisplay form
+            // regenerate select List in view
+            model.RoleList = _roleManager.Roles.Where(x => x.Name != RoleType.Role_Admin).Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+            return View(model);
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Login(string returnUrl = null)
         {
